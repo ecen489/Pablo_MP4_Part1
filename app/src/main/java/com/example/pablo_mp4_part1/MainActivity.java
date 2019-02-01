76013bd86140ff56790fe83b98efd3ae1c4ec247
Pablo Romo <pabromo@tamu.edu>
2019-02-01 00:52:16 -0600

    Done yo

--- app/src/main/java/com/example/pablo_mp4_part1/MainActivity.java
+++ app/src/main/java/com/example/pablo_mp4_part1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.pablo_mp4_part1;
+//Author Pablo
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displaytext(View view) {
+        TextView ot = (TextView) findViewById(R.id.output_text);
+        EditText et = (EditText) findViewById(R.id.editText);
+
+        //Get text
+        String newt = et.getText().toString();
+
+        //Send it back out!~
+        ot.setText(newt);
     }
 }
